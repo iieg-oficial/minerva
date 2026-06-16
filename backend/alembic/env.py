39c7ef327baf6bb84bce77e48a03e6a58bf26742
alejandroiieg <alejandro.zarate@iieg.gob.mx>
2026-06-16 10:40:21 -0600

    feat: add database migrations

---
+++ backend/alembic/env.py
@@ -0,0 +1,44 @@
+from logging.config import fileConfig
+
+from sqlmodel import SQLModel
+
+from alembic import context
+from app.core.config import settings
+from app.core.models import import_models
+
+import_models()
+
+config = context.config
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
+
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+target_metadata = SQLModel.metadata
+
+
+def run_migrations_offline() -> None:
+    context.configure(
+        url=settings.DATABASE_URL,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        compare_type=True,
+    )
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online() -> None:
+    from sqlalchemy import create_engine
+
+    connectable = create_engine(settings.DATABASE_URL)
+    with connectable.connect() as connection:
+        context.configure(connection=connection, target_metadata=target_metadata, compare_type=True)
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
